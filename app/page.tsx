a814b853c73cbebdc61ab2939ca9b0a0d992c0fc
Faizanshaikh12 <faizanroy000777@gmail.com>
2023-08-07 00:12:16 +0530

    complate car details model

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,36 @@
-import {Hero, SearchBar, CustomFilter} from "@/components";
+import { Hero, SearchBar, CustomFilter, CarCard } from "@/components";
+import { fatchCars } from "@/utils";
 
-export default function Home() {
+export default async function Home() {
+    const allCars = await fatchCars();
+    const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars;
+
     return (
         <main className="overflow-hidden">
-            <Hero/>
+            <Hero />
             <div className="mt-12 padding-x padding-y max-width" id="discover">
                 <div className="home__text-container">
                     <h1 className="text-4xl font-extrabold">Car Catalogue</h1>
                     <p>Explore the cars you might like</p>
                     <div className="home__filters">
-                        <SearchBar/>
+                        <SearchBar />
                         <div className="home__filter-container">
-                        <CustomFilter/>
+                            <CustomFilter />
+                            <CustomFilter />
                         </div>
                     </div>
+                    {!isDataEmpty ? (
+                        <section>
+                            <div className="home__cars-wrapper">
+                                {allCars.map((car, index) => <CarCard key={index} car={car}/>)}
+                            </div>
+                        </section>
+                    ) : (
+                        <div className="home__error-container">
+                            <h2 className="text-black text-xl">Oops, no results</h2>
+                            <p>{allCars.message}</p>
+                        </div>
+                    )}
                 </div>
             </div>
         </main>
--- utils/index.ts
+++ utils/index.ts
@@ -1,16 +1,29 @@
-const url = 'https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla';
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': 'f870479df4mshc1059901056db2ap17c64fjsn766e16d8ad1e',
-		'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
-	}
-};
+export async function fatchCars() {
+    const headers = {
+        'X-RapidAPI-Key': 'f870479df4mshc1059901056db2ap17c64fjsn766e16d8ad1e',
+        'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
+    }
+    const url = "https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla"
 
-try {
-	const response = await fetch(url, options);
-	const result = await response.text();
-	console.log(result);
-} catch (error) {
-	console.error(error);
+    const response = await fetch(url, { headers })
+    const result = response.json()
+
+    return result;
+}
+
+export const calculateCarRent = (city_mpg: number, year: number) => {
+    const doller = 80;
+    const basePricePerDay = 50 * doller; // Base rental price per day in ruppe
+    const mileageFactore = 0.1 * doller; // Additional rate per mile driven
+    const ageFactor = 0.05 * doller; // Additional rate per year of vehicle age
+
+    // Calculate additional rate based on mileage and age
+    const mileageRate = city_mpg * mileageFactore;
+    const ageRate = (new Date().getFullYear() - year) * ageFactor;
+
+    // Calculate total rental rate per day
+    const rentalRatePerDay = basePricePerDay + mileageRate + ageRate;
+
+    return rentalRatePerDay.toFixed(0);
+
 }
